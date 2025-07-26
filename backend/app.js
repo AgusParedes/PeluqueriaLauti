68278da68cfac9683ec663e4bc2286e273b1afbf
AGUSTIN PAREDES <paredesagustin15@gmail.com>
2025-07-26 17:28:24 -0300

    front arreglado

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,24 @@ import AdminPanel from './src/routes/admin.router.js';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import db from './db.js'; 
 
 
 dotenv.config(); 
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+db.serialize(() => {
+  const usuarioAdmin = process.env.ADMIN_USER ;
+  const passwordAdmin = process.env.ADMIN_PASS; 
+  db.run(
+    `INSERT OR IGNORE INTO Admins (usuario, password) VALUES (?, ?)`,
+    [usuarioAdmin, passwordAdmin]
+  );
+});
 
 app.use('/login', AdminRouter);
 app.use('/AdminPanel', AdminPanel);
