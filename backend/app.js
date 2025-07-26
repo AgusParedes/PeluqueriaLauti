c801c3e28e1371e24e5afcac34beaedf2036d7dc
AGUSTIN PAREDES <paredesagustin15@gmail.com>
2025-07-26 20:38:00 -0300

    cambio de PORT

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,8 @@ app.use('/AdminPanel', AdminPanel);
 app.use('/api/turnos', turnosRouter);
 
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
